04b0e5398fc0349641389af4939e3edf75ad9be0
zorbeyyavas <yavaszorbey@gmail.com>
2024-10-12 15:58:50 +0300

    Add files via upload

---
+++ sayisalverial.py
@@ -0,0 +1,5 @@
+#python da kullanıcıdan sayısal bir değer almak için inputtan önce int diyip parantez içine almalıyız yoksa metinsel algılar ve sayıları yan yana yazar
+
+a= int(input("sayı giriniz"))
+b= int (input("2.sayı giriniz"))
+print(a+b)
